bcb7115d9f90d1450a0e86bcbb5cc0c5f5f931c6
Matty C <mcarullo@uwaterloo.ca>
2025-12-24 13:45:48 -0500

    Now you can play chess against yourself. Board rep in ASCII.

--- main.py
+++ main.py
@@ -2,7 +2,23 @@ import chess
 
 board = chess.Board()
 
-Nf3 = chess.Move.from_uci("g1f3")
-board.push(Nf3)
+while not board.is_game_over():
 
-print(board)
+    while True:
+        chess_move = input("Enter your move (UCI): ")
+
+        # Error handling for invalid UCI format
+        try:
+            move = chess.Move.from_uci(chess_move)
+        except ValueError:
+            print("Invalid format, try again.")
+            continue
+        # Check if the move is legal
+        if move in board.legal_moves:
+            board.push(move)
+            print(board)
+            break
+        else:
+            print("Illegal move, try again.")
+
+print("Game over:", board.result())
