da8f16472567556f637895dd482a66a0005d13f8
Matty C <mcarullo@uwaterloo.ca>
2025-12-25 13:47:53 -0500

    Added PERFT and verified up to depth=5

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import chess
 import random
 
+def perft(board, depth):
+    if depth == 0:
+        return 1
+
+    nodes = 0
+    for move in board.legal_moves:
+        board.push(move)
+        nodes += perft(board, depth - 1)
+        board.pop()
+    return nodes
+
+def run_perft(depth):
+    print(f"\nRunning PERFT to depth {depth}...\n")
+    result = perft(board, depth)
+    print(f"Nodes: {result}\n")
+
 def human_move():
     while True:
         san_move = input("Enter your move (SAN): ")
@@ -18,6 +34,7 @@ def human_move():
         except ValueError:
             print("Illegal move")
 
+# RandomFish: Chess bot which simply plays random legal moves
 def random_machine_move():
     legal_moves = list(board.legal_moves)
     bot_move = random.choice(legal_moves)
@@ -31,17 +48,20 @@ def random_machine_move():
 board = chess.Board()
 print(board)
 
+# Run PERFT test at depth 1
+run_perft(3)
+
+
 white_or_black = input("Choose your side (white/black): ").strip().lower()
 if white_or_black == "white":
     while not board.is_game_over():
-
-        # --- White to move: Human ---
+        # White to move: Human
         human_move()
 
         if board.is_game_over():
             break
 
-        # --- Black to move: Random AI ---
+        # Black to move: Random AI
         machine_move = random_machine_move()
 
         print("\n" + str(board) + "\n")
@@ -50,7 +70,7 @@ if white_or_black == "white":
     print("Game over:", board.result())
 else:
     while not board.is_game_over():
-        # --- Black to move: Random AI ---
+        # White to move: Random AI
         machine_move = random_machine_move()
 
         print("\n" + str(board) + "\n")
@@ -59,7 +79,7 @@ else:
         if board.is_game_over():
             break
 
-        # --- White to move: Human ---
+        # Black to move: Human
         human_move()
     
     print("Game over:", board.result())
